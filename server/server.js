92b7da121b53204367b3a491e786b3a57b9a9175
Jimmy Bastos <jimmybastosdev@gmail.com>
2021-01-23 19:51:27 -0300

    Add mock server timestamps

--- server/server.js
+++ server/server.js
@@ -5,6 +5,24 @@ const middlewares = jsonServer.defaults()
 const port = process.env.PORT || 3000
 
 server.use(middlewares)
+
+server.use(jsonServer.bodyParser)
+
+server.use((req, res, next) => {
+  if (req.method === 'POST') {
+    req.body.created_at = new Date()
+    req.body.updated_at = new Date()
+  }
+  next()
+})
+
+server.use((req, res, next) => {
+  if (req.method === 'PUT' || req.method === 'PATCH') {
+    req.body.updated_at = new Date()
+  }
+  next()
+})
+
 server.use(router)
 
 server.listen(port)
